71a7406cf955a06e1c249fc23144f21f2175b25a
ElMondirr <152962192+Elmondirr@users.noreply.github.com>
2024-04-24 12:35:29 +0200

    Maven

---
+++ escaneo/src/test/java/com/example/escaneo/ContadorDeEntradasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.escaneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorDeEntradasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
